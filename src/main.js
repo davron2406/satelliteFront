f3ae5f24c0d9569320ff7d2a2be4c350ac843b63
profix <davronsaydullayev2406@gmail.com>
2024-11-29 00:11:18 +0500

    changed

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import axios from 'axios'
 import router from './router'
 
-axios.defaults.baseURL = 'https://satelliteback.onrender.com';
+axios.defaults.baseURL = 'https://satelliteback.onrender.com/api';
 
 const app = createApp(App).use(router)
 
